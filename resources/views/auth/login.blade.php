1e03a07e17879693f30fc9d58545ee8e50b7bada
Daniel Pett <dejp3@cam.ac.uk>
2021-03-06 08:12:30 +0000

    Amend password form titles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12 mb-3 mt-3">
         <div class="card mx-4">
             <div class="card-body p-4">
-                <h1>{{ trans('panel.site_title') }}</h1>
+                <h1>Login to the Museum's room booker</h1>
 
                 <p class="text-muted">{{ trans('global.login') }}</p>
 
