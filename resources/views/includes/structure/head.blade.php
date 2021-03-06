261251ae118ec8b9071448245264dd809f067c10
Daniel Pett <dejp3@cam.ac.uk>
2021-03-06 09:26:19 +0000

    Amend header

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,3 +1,9 @@
-<div class="container-fluid head parallax ">
-    <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" class="img-fluid" width="500">
+<div class="jumbotron jumbotron-fluid mb-5 doc-jumbotron head">
+  <div class="container">
+    <div class="row ">
+      <div class="col-lg-12 col-xl-12 ">
+        <a href="https://www.fitzmuseum.cam.ac.uk"><img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" class="align-self-center mr-3 float-left thumb-post  p-3 " width="300"></a>
+      </div>
+    </div>
+  </div>
 </div>
